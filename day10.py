71ff93be90721e0f56c1e057ecc5b9d0d5aff7bc
Colin Cummins <all.our.august.sundays@gmail.com>
2022-12-10 12:13:09 -0800

    Need to revise get_adders so instructions are being carried out simultaneously

---
+++ day10.py
@@ -0,0 +1,61 @@
+# Day 10 - Signal Strengths
+def get_adders(filename: str) -> list[int]:
+    """
+    Returns an array of the amount to add to signal strength at the _start_ of the cycle
+    :param filename:
+    :return:
+    """
+    with open(filename, "r") as fo:
+        adders = []
+        for line in fo.readlines():
+            instruction, *amount = line.split()
+            adders.append(0)
+            if amount:
+                adders.append(int(amount[0]))
+    print(adders)
+    return adders
+
+def get_registers(adders):
+    register = 1
+    registers = []
+    for adder in adders:
+        register += adder
+        registers.append(register)
+
+    return registers
+
+def get_signal_strengths(registers):
+    strengths = []
+    for cycle, register in enumerate(registers):
+        cycle += 1
+        strengths.append(cycle * register)
+        print(f"Cycle: {cycle}, Register: {register}, Strength: {strengths[-1]}")
+    return strengths
+
+def filter_signal_strengths(strengths):
+    # Take list of signal strengths and only return target strengths
+    from itertools import count
+    filtered = []
+    target = 20
+    while target < len(strengths):
+        idx = target - 1
+        print(f"Target Cycle: {idx + 1}, Signal Strength: {strengths[idx]}")
+        filtered.append(strengths[idx])
+        target += 40
+
+
+    return filtered
+
+
+
+def prob1(filename: str) -> int:
+    """
+    Find the signal strength during the 20th, 60th, 100th, 140th, 180th, and 220th cycles.
+    What is the sum of these six signal strengths?
+    :param filename: input file
+    :return: Sum of signal strengths at target cycles
+    """
+    print(sum(filter_signal_strengths(get_signal_strengths(get_registers(get_adders(filename))))))
+
+
+prob1("test_input.txt")
