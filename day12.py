d5780998dfb8bf9848fa4a7f853b19b6530213b7
Colin Cummins <all.our.august.sundays@gmail.com>
2022-12-12 19:33:14 -0800

    Part 1 Complete

--- day12.py
+++ day12.py
@@ -1,87 +1,116 @@
 # Advent of Code 2022 - Day 12
+import numpy as np
 def create_grid(filename: str):
-  # Create grid from AoC input
-  with open(filename, "r") as fo:
-    rows = fo.read().split("\n")
-    Grid = [[0 for i in range(len(rows[0]))] for _ in range(len(rows))]
-
-
-create_grid("Test_input.txt")
+    # Create grid from AoC input
+    # return grid and tuples for start/end points
+    start, end = None, None
+    with open(filename, "r") as fo:
+        rows = fo.read().split("\n")
+        grid = [[0 for i in range(len(rows[0]))] for _ in range(len(rows))]
+    for i in range(len(rows)):
+        for j in range(len(rows[0])):
+            if rows[i][j] == "S":
+                start = i, j
+                grid[i][j] = 0
+            elif rows[i][j] == "E":
+                end = i, j
+                grid[i][j] = ord("z") - ord("a")
+            else:
+                grid[i][j] = ord(rows[i][j]) - ord("a")
+    print("Grid created:", grid)
+    return grid, start, end
 
 
 def solve_puzzle(Board, Source, Destination):
-  """
+    """
     Navigates the maze Board from Source to Destination
     :param Board: Board to navigate list[list[str]]
     :param Source: (Row, Column) of starting point - tuple[int,int]
     :param Destination: (Row, Column) of goal - tuple[int,int]
     :return: List of coordinates of the path to goal, in order. Returns None if no path to goal
     """
-  from heapq import heappush, heappop
+    from heapq import heappush, heappop
 
-  dist = [[float("inf")] * len(Board[0]) for _ in range(len(Board))]
+    print(Board, Source, Destination)
 
-  prev = [[None] * len(Board[0]) for _ in range(len(Board))]
+    dist = [[float("inf")] * len(Board[0]) for _ in range(len(Board))]
 
-  dist[Source[0]][Source[1]] = 0
-  prev[Source[0]][Source[1]] = Source
+    prev = [[None] * len(Board[0]) for _ in range(len(Board))]
 
-  pq = [(0, Source[0], Source[1])]  # tentative_dist, row, col
+    dist[Source[0]][Source[1]] = 0
+    prev[Source[0]][Source[1]] = Source
 
-  path = []
-  visited = set()
+    pq = [(0, Source[0], Source[1])]  # tentative_dist, row, col
 
-  # print("Source: ", Source)
-  # print("Dest: ", Destination)
-  # print(*Board, sep='\n')
+    path = []
+    visited = set()
 
-  def is_in_bounds(r, c):
-    return -1 < r < len(Board) and -1 < c < len(Board[0])
+    # print("Source: ", Source)
+    # print("Dest: ", Destination)
+    # print(*Board, sep='\n')
 
-  while pq:
-    distance, row, col = heappop(pq)
-    if (row, col) not in visited and Board[row][col] != "#":
-      # print("visited", row, col)
-      visited.add((row, col))
-      dist[row][col] = distance
-      for neighbor in [(row + 1, col), (row - 1, col), (row, col + 1),
-                       (row, col - 1)]:
-        r, c = neighbor
-        if is_in_bounds(r, c) and dist[row][col] + 1 < dist[r][c]:
-          dist[r][c] = dist[row][col] + 1
-          prev[r][c] = (row, col)
-          heappush(pq, (dist[r][c], r, c))
+    def is_in_bounds(r, c):
+        return -1 < r < len(Board) and -1 < c < len(Board[0])
 
-  # Build up path in reverse
-  curr = Destination
-  while curr != Source:
-    path.append(curr)
-    r, c = curr
-    curr = prev[r][c]
-  path.append(Source)
-  path.reverse()
-  print(path)
+    def is_reachable(row, col, r, c):
+        return Board[r][c] - Board[row][col] < 2
 
-  # Create direction string for extra credit
-  directions = []
-  shift_to_direction = {(1, 0): "D", (-1, 0): "U", (0, 1): "R", (0, -1): "L"}
-  for i in range(len(path) - 1):
-    pr1, pc1 = path[i]
-    pr2, pc2 = path[i + 1]
-    current_direction = (pr2 - pr1, pc2 - pc1)
-    # print(current_direction)
-    # print(shift_to_direction[current_direction])
-    directions.append(shift_to_direction[current_direction])
-  direction_string = "".join(directions)
+    while pq:
+        distance, row, col = heappop(pq)
+        if (row, col) not in visited:
+            # print("visited", row, col)
+            visited.add((row, col))
+            dist[row][col] = distance
+            for neighbor in [(row + 1, col), (row - 1, col), (row, col + 1), (row, col - 1)]:
+                r, c = neighbor
+                if is_in_bounds(r, c) and is_reachable(row, col, r, c) and dist[row][col] + 1 < dist[r][c]:
+                    dist[r][c] = dist[row][col] + 1
+                    prev[r][c] = (row, col)
+                    heappush(pq, (dist[r][c], r, c))
 
-  return path, direction_string
+    # Build up path in reverse
+    curr = Destination
+    while curr != Source:
+        path.append(curr)
+        r, c = curr
+        curr = prev[r][c]
+    path.append(Source)
+    path.reverse()
+
+    # Create direction string
+    directions = []
+    shift_to_direction = {(1, 0): "D", (-1, 0): "U", (0, 1): "R", (0, -1): "L"}
+    for i in range(len(path) - 1):
+        pr1, pc1 = path[i]
+        pr2, pc2 = path[i + 1]
+        current_direction = (pr2 - pr1, pc2 - pc1)
+        # print(current_direction)
+        # print(shift_to_direction[current_direction])
+        directions.append(shift_to_direction[current_direction])
+    direction_string = "".join(directions)
+
+    return path, direction_string
 
 
-Puzzle = [['-', '-', '-', '-', '-'], ['-', '-', '#', '-', '-'],
-          ['-', '-', '-', '-', '-'], ['#', '-', '#', '#', '-'],
-          ['-', '#', '-', '-', '-']]
-print(solve_puzzle(Puzzle, (0, 2), (2, 2)))
-Puzzle = [['-', '-', '-', '-', '-'], ['-', '-', '#', '-', '-'],
-          ['-', '-', '-', '-', '-'], ['#', '-', '#', '#', '-'],
-          ['-', '#', '-', '-', '-']]
-print(solve_puzzle(Puzzle, (0, 0), (4, 4)))
+def prob1(filename):
+    board, start, end = create_grid(filename)
+    path, directions = solve_puzzle(board, start, end)
+    print("Board: ", board)
+    print(f"Start: {start}     End: {end}")
+    print("Path: ", path)
+    print("Steps: ", len(path) - 1)
+    print("Directions: ", directions)
+
+
+def prob2(filename):
+    best_path = float("inf")
+    board, start, end = create_grid(filename)
+
+    path, directions = solve_puzzle(board, start, end)
+    print("Board: ", board)
+    print(f"Start: {start}     End: {end}")
+    print("Path: ", path)
+    print("Steps: ", len(path) - 1)
+    print("Directions: ", directions)
+
+prob1("input.txt")
