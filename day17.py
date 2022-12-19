40b72823c0b4e7945291abfee3767c5f0f9a2c97
Colin Cummins <all.our.august.sundays@gmail.com>
2022-12-18 19:41:17 -0800

    Shadow creation works but is not yet being applied to boards before next turn

--- day17.py
+++ day17.py
@@ -3,8 +3,11 @@
 # Pyroclastic flow
 # Bit manipulation and cycle detection
 from itertools import cycle
+from functools import cache
+
+
 class Turn:
-    def __init__(self, turn_number: int, instruction_num: int, block_type: int, board: int, height:int, next):
+    def __init__(self, turn_number: int, instruction_num: int, block_type: int, board: int, height: int, next):
         self.turn_number = turn_number
         self.instruction_num = instruction_num
         self.block_type = block_type
@@ -17,7 +20,7 @@ class Turn:
 
     def __repr__(self):
         return f"Turn: {self.turn_number}\nInstruction: {self.instruction_num}\nBlock Type: {self.block_type}\nBoard" \
-               f":\nCurrent Height:{self.height}{Volcano.render(None,self.board)} "
+               f":\nCurrent Height:{self.height}{Volcano.render(None, self.board)} "
 
 
 class Volcano:
@@ -39,7 +42,6 @@ class Volcano:
 
         self.INSTRUCTION_ITER = cycle(range(len(self.INSTRUCTION_SEQ)))
         self.BLOCK_ITER = cycle(range(len(self.BLOCK_ORDER)))
-
 
     def test_pattern(self):
         print("Horizontal 4:")
@@ -78,7 +80,7 @@ class Volcano:
         """
         return (profile << 49) + (test.STARTING_SIGNATURE << 56)
 
-    def move_block(self, block_type: int, block_bits: int, direction: str, board: int) -> [int, bool]:
+    def move_block(self, block_type: int, block_bits: int, direction: str, board: int, debug=None) -> [int, bool]:
         """
         Gas jets push the block left or right, unless it will run into a wall. Then the block tries to drop.
         If the block can't drop, it comes to rest.
@@ -87,17 +89,75 @@ class Volcano:
         :param direction:
         :return: (new block [int], status of block - True: Still falling, False - resting)
         """
-        if direction == ">" and ((block_bits) % self.ROW_MOD) != self.BLOCK_RIGHT[block_type]:
+        if direction == ">" and ((block_bits % self.ROW_MOD) != self.BLOCK_RIGHT[block_type]) and (
+                (block_bits << 1) & board == 0):
             block_bits <<= 1
+            if debug:
+                self.render(board + block_bits)
 
-        if direction == "<" and ((block_bits) % self.ROW_MOD) != self.BLOCK_LEFT[block_type]:
+        if direction == "<" and ((block_bits % self.ROW_MOD) != self.BLOCK_LEFT[block_type]) and (
+                (block_bits >> 1) & board == 0):
             block_bits >>= 1
+            if debug:
+                self.render(board + block_bits)
 
         if (block_bits << 7) & board == 0:
             block_bits <<= 7
+            if debug:
+                self.render(board + block_bits)
             return block_bits, True
 
         return block_bits, False
+
+    @cache
+    def simplify_board(self, board: int) -> int:
+        """
+        Takes a board and simplifies it by reducing it to the 'shadow' of every possible space the
+        :param board:
+        :return:
+        """
+        shadow = 0
+        stack = []
+        for block_type in range(5):
+            block_bits = self.BLOCK_ORDER[block_type]
+            stack.append((block_bits, ">"))
+            stack.append((block_bits, "<"))
+            while stack:
+                block_bits, direction = stack.pop()
+                if direction == ">" and ((block_bits % self.ROW_MOD) != self.BLOCK_RIGHT[block_type]) and (
+                        (block_bits << 1) & board == 0):
+                    block_bits <<= 1
+                    shadow |= block_bits
+
+                if direction == "<" and ((block_bits % self.ROW_MOD) != self.BLOCK_LEFT[block_type]) and (
+                        (block_bits >> 1) & board == 0):
+                    block_bits >>= 1
+                    shadow |= block_bits
+
+                if (block_bits << 7) & board == 0:
+                    block_bits <<= 7
+                    shadow |= block_bits
+                    stack.append((block_bits, ">"))
+                    stack.append((block_bits, "<"))
+
+        # Chop off top two rows which have artifacts d/t starting positions of shapes
+        shadow = shadow >> 14
+
+        # Convert to board format
+        mask = 2 ** shadow.bit_length()
+        shadow = shadow ^ mask - 1
+        while shadow & self.ROW_MOD == 0:
+            shadow >>= 7
+
+        # Add a floor to the bottom
+        add_row = shadow.bit_length() // 7 + 1
+        print(add_row)
+        shadow = shadow + (self.STARTING_SIGNATURE << (7 * add_row))
+
+        # Push down to make space for staging area and drop region
+        shadow = shadow << 49
+        self.render(shadow)
+        return shadow
 
     def take_turn(self, board: int):
         """
@@ -110,7 +170,6 @@ class Volcano:
         current_block = self.BLOCK_ORDER[current_block_num]
         falling = True
         while falling:
-            self.render(board + current_block)
             current_instruction = self.INSTRUCTION_SEQ[next(self.INSTRUCTION_ITER)]
             current_block, falling = self.move_block(current_block_num, current_block, current_instruction, board)
         board = board + current_block
@@ -120,17 +179,17 @@ class Volcano:
         while board & self.ROW_MOD == 0:
             board >>= 7
             height_adder -= 1
-            self.render(board)
 
         # Push board back to make room for next block to fall
         board <<= 49
 
+        # reduce board to its simplest form
+        # board = self.simplify_board(board)
 
-        return board
+        return board, height_adder
 
 
-
-test = Volcano("input.txt")
+test = Volcano("test_input.txt")
 
 # Drop test code
 # for blocknum in range(5):
@@ -179,6 +238,11 @@ test = Volcano("input.txt")
 #         i += 1
 
 my_board = (test.STARTING_SIGNATURE << 56) + (test.STARTING_SIGNATURE << 49)
-for i in range(10):
-    my_board = test.take_turn(my_board)
+height = 0
+for i in range(5):
+    my_board, height_adder = test.take_turn(my_board)
+    # test.render(my_board)
+    height += height_adder
     test.render(my_board)
+    simple = test.simplify_board(my_board)
+print(height)
