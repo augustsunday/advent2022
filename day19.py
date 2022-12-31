e97aadd7f1ba4a5e3b3d44339cf5d4dc01422262
Colin Cummins <all.our.august.sundays@gmail.com>
2022-12-30 21:32:04 -0800

    Trying dfs solution again, but need to adjust pruning on line 63

--- day19.py
+++ day19.py
@@ -1,4 +1,5 @@
 # Advent of Code 2022 - Day 19 - Not Enough Minerals
+from functools import cache
 
 
 class Solution:
@@ -11,6 +12,10 @@ class Solution:
         self.current_blueprint = None
         self.optimal_blueprint = None
 
+        self.max_cost = None
+
+
+    @cache
     def dfs(self, turn, resources, bots):
         """
         Return max geodes obtainable with available bots, resources, and remaining time
@@ -29,12 +34,7 @@ class Solution:
 
         # put in pruning conditions
         # Local - Were we able to get to this turn/geode/resource/bot combo locally in less time? Return 0
-        print(resources)
-        if (turn, resources, bots) in self.local_best:
-            return resources[-1]
-
-        self.local_best[(turn, resources, bots)] = resources[-1]
-
+        print(bots, resources)
 
         if turn == 25:
             return resources[-1]
@@ -42,18 +42,34 @@ class Solution:
         # Step 1 - Produce
         resources = [x + y for x, y in zip(resources, bots)]
 
+
         # Step 2 - Try making each kind of bot and recurse. Return best geode production.
         best_production = 0
-        for idx, botcost in enumerate(self.current_blueprint):
-            if all(cost <= resource for cost, resource in zip(botcost, resources)):
-                tmp_resource = tuple([resource - cost for cost, resource in zip(botcost, resources)])
-                tmp_bots = list(bots)
-                tmp_bots[idx] += 1
-                tmp_bots = tuple(tmp_bots)
-                best_production = max(best_production, self.dfs(turn + 1, tmp_resource, tmp_bots))
+        could_produce = False
 
-        # Try producing nothing this turn (we need the option to save ore for building obsidian or geode bots rather than more orebots)
-        best_production = max(best_production, self.dfs(turn + 1, tuple(resources), bots))
+        # If we can make geode bots, we should
+        if all(cost <= resource for cost, resource in zip(self.current_blueprint[3], resources)):
+            could_produce = True
+            tmp_resource = tuple([resource - cost for cost, resource in zip(self.current_blueprint[3], resources)])
+            tmp_bots = list(bots)
+            tmp_bots[3] += 1
+            tmp_bots = tuple(tmp_bots)
+            best_production = max(best_production, self.dfs(turn + 1, tmp_resource, tmp_bots))
+
+
+        # Otherwise try scnearios producing each kind of bot, but only if we need more bots to meet our highest cost
+        else:
+            for idx, botcost in enumerate(self.current_blueprint):
+                if all(cost <= resource for cost, resource in zip(botcost, resources)) and resources[idx] < self.max_cost[idx]:
+                    could_produce = True
+                    tmp_resource = tuple([resource - cost for cost, resource in zip(botcost, resources)])
+                    tmp_bots = list(bots)
+                    tmp_bots[idx] += 1
+                    tmp_bots = tuple(tmp_bots)
+                    best_production = max(best_production, self.dfs(turn + 1, tmp_resource, tmp_bots))
+
+        if not could_produce:
+            best_production = max(best_production, self.dfs(turn + 1, tuple(resources), bots))
 
         return best_production
 
@@ -63,4 +79,5 @@ test = Solution()
 resource = (0, 0, 0, 0)
 bots = (1, 0, 0, 0)
 test.current_blueprint = ((4, 0, 0, 0), (2, 0, 0, 0), (3, 14, 0, 0), (2, 0, 17, 0))
+test.max_cost = (4, 14, 17, 0)
 print(test.dfs(1, resource, bots))
